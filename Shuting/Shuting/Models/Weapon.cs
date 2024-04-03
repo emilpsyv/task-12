ad1933bfcd298618478a4f239b4de063841b23c0
emilpsyv <161576137+emilpsyv@users.noreply.github.com>
2024-04-03 05:29:17 +0400

    task added

---
+++ Shuting/Shuting/Models/Weapon.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shuting.Models
+{
+    internal class Weapon
+    {
+        public string WeaponName;
+        private int _bulletCapacity;
+        private int _bulletInComb;
+        private double _dechargeTime;
+        private string _fireMode;
+
+
+        public int BulletCapacity 
+        {
+            get
+            { 
+                return _bulletCapacity;
+            }
+            set
+            {
+                if (value > 0) 
+                {
+                    _bulletCapacity = value;
+                }
+                else { Console.WriteLine("gulle menfi ola bilmez"); }
+
+            }
+        
+        
+        }
+
+        public int BulletInComb 
+        {
+            get
+            {
+                return _bulletInComb;
+            }
+                
+            set
+            {  
+                if (value > 0 && value < BulletCapacity)
+                
+                {
+                    _bulletInComb = value;
+                }
+                else { Console.WriteLine("gulle sayi daragin tutumunnan cox ola bilmez"); }
+            }
+        }
+
+        public double DechargeTime 
+        {
+            get
+            {
+                return _dechargeTime;
+            }
+            
+            
+            set
+            {
+                if (value > 0) 
+                {
+                    double time =  (_bulletCapacity / _bulletInComb);//bu hisse tam duz islemir 
+                    _dechargeTime = time*(value);
+                }
+                else { Console.WriteLine("vaxt menvi ve ya 0 ola bilmez"); }
+            }
+        
+        }
+
+        public string FireMode 
+        {
+            get { return _fireMode; }
+
+
+          
+            set {
+                if (value == "single")
+                {
+                    _fireMode = value;
+                }
+                else if (value == "auto")
+                {
+                    _fireMode = value;
+                }
+                else { Console.WriteLine("bele bir mod movcud deyil"); }
+            
+            }
+
+            //buda ChangeFireModeki commite aiddir
+            //set
+            //{
+            //  ChangeFireMode(value);
+            //}
+
+
+        }
+
+
+        public Weapon(string name,int bulletCapacity, int bulletInComb, double dechargeTime, string fireMode)
+        {
+            WeaponName= name;
+            BulletCapacity= bulletCapacity;
+            BulletInComb= bulletInComb;
+            DechargeTime = dechargeTime;
+            FireMode = fireMode;
+                
+
+        }
+
+
+
+        public void WeaponInfo() 
+        {
+            Console.WriteLine($"silah adi: {WeaponName}\ngulle tutumu: {_bulletCapacity}\nqalmis gulle sayi: {_bulletInComb}\ndaragin tam bosalma vaxti: {_dechargeTime} saniye\nates modu: {FireMode}    " );
+        }
+    
+        public void Shoot()
+        {
+            if (_bulletInComb >0) 
+            {
+                _bulletInComb--;
+                Console.WriteLine($"piu piu\nqalan gulle sayi: {_bulletInComb}");
+
+
+            }
+            else
+            {
+                Console.WriteLine("darag bosdur");
+            }
+        }
+
+        public void Fire() 
+        {
+            if (_bulletInComb>0) 
+            { 
+                _bulletInComb=0;
+                Console.WriteLine($"daragda qalmis gulle sayi: {_bulletInComb}\ndaragin bosalma vaxti: {_dechargeTime} saniye " );
+                
+            }
+            else
+            { Console.WriteLine("darag bosdur"); }
+        }
+        
+        public int GetRemainBulletCount()
+        {
+
+            return _bulletCapacity - _bulletInComb;
+        }
+
+        public void Reload()
+        {
+            _bulletInComb= _bulletCapacity;
+            Console.WriteLine(_bulletInComb);
+
+        }
+
+        public void ChangeFireMode()
+        {
+            Console.WriteLine("1.single\n2.auto\n1 veya 2 daxil edin");
+            int a = int.Parse(Console.ReadLine());
+            switch (a)
+            {
+
+                case 1:
+                    Console.WriteLine("single ates modu secildi");
+                    _fireMode = "single";
+                    break;
+                case 2:
+                    Console.WriteLine("auto ates modu secildi");
+                    _fireMode = "auto";
+                    break;
+            }
+
+
+
+            //bunu tam duz qos ede bilmedim 
+            //if (FireMode=="single")
+            //{
+            //    Shoot();
+            //    Console.WriteLine($"Firing mode changed to {FireMode}.");
+            //}
+            //else if (FireMode=="auto")
+            //{
+            //    Fire();
+            //    Console.WriteLine($"Firing mode changed to {FireMode}.");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("not correct mode");
+            //}
+        }
+
+        public void T()
+        {
+            Console.WriteLine("yeni darag tutumun daxil edin");
+            BulletCapacity = int.Parse(Console.ReadLine());
+        }
+
+        public void S()
+        {
+            Console.WriteLine("yeni gulle sayin daxil edin");
+            BulletInComb= int.Parse(Console.ReadLine());
+
+        }
+
+        public void D()
+        {
+            Console.WriteLine("yeni gulle sifirlanma vaxtin daxil edin");
+            DechargeTime = int.Parse(Console.ReadLine());
+        }
+        public void N()
+        {
+            Console.WriteLine("silahin yeni adin daxil edin");
+            WeaponName = Console.ReadLine();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
---
+++ Shuting/Shuting/Program.cs
@@ -0,0 +1,135 @@
+﻿using Shuting.Models;
+
+namespace Shuting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon("akm", 30, 25, 10, "single");
+            
+           
+
+
+            bool result = true;
+            do
+            {
+                
+                Console.Clear();
+                Console.WriteLine(@"0 - İnformasiya
+1 - Shoot metodu
+2 - Fire metodu 
+3 - Daragda gulle sayi 
+4 - Reload 
+5 - Ates modu 
+6 - Proqrami dayandirmaq 
+7 - Redakte et");
+                int a=int.Parse(Console.ReadLine());
+                if (a < 8)
+                {
+
+                    switch (a)
+                    {
+                        case 0:
+                            weapon.WeaponInfo();
+
+                            break;
+
+                        case 1:
+                            weapon.Shoot();
+                            break;
+                        case 2:
+                            weapon.Fire();
+                            break;
+                        case 3:
+                            weapon.GetRemainBulletCount();
+                            break;
+                        case 4:
+                            weapon.Reload();
+                            break;
+                        case 5:
+                            weapon.ChangeFireMode();
+                            break;
+                        case 6:
+                            result = false;
+                            break;
+                        case 7:
+                            Console.WriteLine(@"T - Tutumu deyis
+S - Gülle sayin deyis
+D - Sifirlama saniyesin deyis
+N - Silahin adin deyis");
+                            char Letter = char.Parse(Console.ReadLine());
+                            switch (Letter)
+                            {
+                                case 'T':
+                                    weapon.T();
+                                    break;
+
+                                case 'S':
+                                    weapon.S();
+                                    break;
+
+                                case 'D':
+                                    weapon.D();
+                                    break;
+
+                                case 'N':
+
+                                    break;
+
+
+
+
+                            }
+
+                            break;
+
+                    }
+                    Console.WriteLine(@"
+
+
+
+
+
+davam etmek ucun 'enter' duymesin basin ");
+                    Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+                }
+                else 
+                {
+                    Console.WriteLine("menuda bele bir secim yoxdur,zehmet olmasa secimi duzgun edin");
+                }
+                Console.WriteLine("\ndavam etmek ucun 'enter' basin");
+                Console.ReadLine();
+
+            }
+            while (result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
